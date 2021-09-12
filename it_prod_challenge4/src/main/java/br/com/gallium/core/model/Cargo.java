57f5221230688d84eaf7b85884908beb8ae02276
gu2901 <gu2901@hotmail.com>
2021-09-12 15:51:35 -0300

    Delete it_prod_challenge4 directory

--- it_prod_challenge4/src/main/java/br/com/gallium/core/model/Cargo.java
+++
@@ -1,9 +0,0 @@
-package br.com.gallium.core.model;
-
-public enum  Cargo {
-    ATENDENTE,
-    ESTOQUISTA,
-    FAXINEIRO,
-    GERENTE,
-    VENDEDOR
-}
--- it_prod_challenge4/src/main/java/br/com/gallium/core/model/Categoria.java
+++
@@ -1,7 +0,0 @@
-package br.com.gallium.core.model;
-
-public enum Categoria {
-    AUTONOMA,
-    FRANQUIA,
-    MATRIZ
-}
